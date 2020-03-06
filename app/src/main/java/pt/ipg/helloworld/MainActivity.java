46b71d1832e2830352292273a2636e4d4023dc01
Tiago Cunha <1700597@sal.ipg.pt>
2020-03-06 10:06:56 +0000

    Interface da main activity

--- app/src/main/java/pt/ipg/helloworld/MainActivity.java
+++ app/src/main/java/pt/ipg/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package pt.ipg.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enviaMensagem(View view) {
     }
 }
